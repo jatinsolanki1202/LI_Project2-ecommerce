e3813cde1fb8081b018f3ef6cbaaf0aa7d51fe33
jatinsolanki <jatin.solanki@lucentinnovation.com>
2025-05-23 00:08:20 +0530

    change minor

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ dotenv.config()
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
