03b5ce5f8669ff615433d4662a2fc3bc5bd5edf5
jatinsolanki <jatin.solanki@lucentinnovation.com>
2025-05-22 23:08:42 +0530

    minor change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import './index.css'
 import App from './App.jsx'
 import { CartContextProvider } from './context/CartContext.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ContextProvider>
