a0260904c9a45e1f3267ac4cc1430c230ff984e7
jatinsolanki <jatin.solanki@lucentinnovation.com>
2025-05-24 22:08:52 +0530

    added vercel json

--- server/index.js
+++ server/index.js
@@ -16,25 +16,14 @@ import loginAuth from './middlewares/loginAuth.js';
 import { getAllProducts, getSingleProduct } from "./controllers/product.controller.js"
 
 syncDatabase()
-// import syncDatabase from './sync.js'
 
 const app = express()
 dotenv.config()
 
-// const corsOpts = {
-//   origin: 'https://ecommerce-project-1-client.vercel.app',
-// };
-
-app.use(cors({ origin: 'https://ecommerce-project-1-client.vercel.app' }));
-// app.options('*', cors(corsOpts));
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
